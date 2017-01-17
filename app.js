29e7e824740fd8761f58079fdb188a64501f5695
unicornelia <ez.kornelia@gmail.com>
2017-01-17 19:21:37 +0000

    Getting very close to our MVP

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var geolocate = require('./routes/geolocate');
+var pubs = require('./routes/pubs');
 
 var app = express();
 
@@ -25,7 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/geolocate', geolocate);
+app.use('/pubs', pubs);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/geolocate.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET users listing. */
-router.get('/geolocate', function(req, res) {
-    res.render('geolocate', { title: 'Geolocate' });
-});
-
-module.exports = router;
