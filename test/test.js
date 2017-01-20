1ab6618ac20a0d11dc6e50a4c4d2206b3896f569
unicornelia <ez.kornelia@gmail.com>
2017-01-20 12:30:00 +0000

    Added test suite mocha and chai, tests are passing for GET request and basic site content

---
+++ test/test.js
@@ -0,0 +1,37 @@
+var expect = require('chai').expect;
+var http = require('http');
+var server = require('../bin/www');
+
+describe('server', function () {
+  before(function () {
+    server.listen(3000);
+  });
+
+  after(function () {
+    server.close();
+  });
+});
+
+describe('/', function () {
+  it('should return 200', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      expect(res.statusCode).to.equal(200);
+      done();
+    });
+  });
+
+  it('should say "Hello there"', function (done) {
+    http.get('http://localhost:3000', function (res) {
+      var data = '';
+
+      res.on('data', function (chunk) {
+        data += chunk;
+      });
+
+      res.on('end', function () {
+        expect(data).to.include('Hello!');
+        done();
+      });
+    });
+  });
+});
