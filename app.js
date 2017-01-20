b60db052f126e945c8f3e8ad4f6792a28d0b3699
unicornelia <ez.kornelia@gmail.com>
2017-01-20 11:09:11 +0000

    Cleaned up master, app is running fine

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var pubs = require('./routes/pubs');
 
 var app = express();
 
@@ -25,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/pubs', pubs);
 
 
 // catch 404 and forward to error handler
